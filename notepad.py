c0926a9324f8deb40085d3c9221a05b9cec691a0
Srikara Narasimha <srikaras07@gmail.com>
2021-12-30 19:32:02 +0530

    Create notepad.py

---
+++ notepad.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+root = Tk()
+root.geometry('1280x720')
+
+frame = Frame(root).place(x=0, y=0)
+
+entry = Text(frame, height = 30, width = 125).place(x=125, y=90)
+
+
+
+
+
+
+
+root.mainloop()
